5e734fe51d3b0ad68917aabd5d5a2d5c9841ed1d
K1NGAARON <gerardaaron1997@gmail.com>
2023-05-31 16:34:27 +0200

    sunday sizer scrip

---
+++ sunday-sizer/js/app.js
@@ -0,0 +1,20 @@
+const sizeChart = [];
+const popUpBtn = document.querySelector('#popUp');
+const neededPattern = document.querySelector('#pattern'); // => pull data from custom type
+
+let chestInput;
+let preferredFit;
+
+
+function showPopUp(e) {
+    
+};
+
+function closePopUp(e) {
+
+};
+
+function calculateSize(e) {
+
+};
+
