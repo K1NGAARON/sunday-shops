c055b779ac1ae52a38daacdefccf8ad20c5216fc
K1NGAARON <gerardaaron1997@gmail.com>
2023-06-05 16:35:52 +0200

    changes

--- js/faq.js
+++ js/faq.js
@@ -1,5 +1,9 @@
 const faq = [
     {
+        question: "What if my size could not be calculated?",
+        answer: "Please send an email to shop-support@wardrobe.teamsunday.com with your chest measurement and if you want a looser fit or not."
+    },
+    {
         question: "Who to contact in case of questions, returns, faulty items, sizing, cancellations, logistical, shop related questions?",
         answer: "Please contact the Sunday customer service team by sending a mail to shop-support@wardrobe.teamsunday.com",
     },
@@ -12,17 +16,13 @@ const faq = [
         answer: "You can only purchase 1 clothing item. If you try to order more than 1 product item, you won’t be able to place an order.",
     },
     {
-        question: "Do I need to choose my animal decoration?",
-        answer: "No, the type of embroidery varies depending on your number of years of employment with Protime & will be distributed by us. The number of different designs is just to show an example of the number of sweaters.",
-    },
-    {
         question: "How does the sizing work?",
         answer: "You only need to measure chest & waist so we can calculate your perfect size fit. This means you won’t need to choose the size when placing your order.",
     },
     {
         question: "Will I receive a confirmation?",
         answer: "When your order is placed, you will receive an order confirmation via e-mail. Please check your spam folder if you don’t receive this confirmation.",
-    }
+    },
 ];
 
 function buildFAQ(e) {
@@ -48,6 +48,39 @@ function buildFAQ(e) {
     };
 };
 
+function addSizeInfoCheckout(e) {
+    const target = document.querySelector('.woocommerce-form-coupon-toggle');
+    const template = `
+        <div class="woocommerce-form-coupon-toggle" style="
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: var(--yellow);
+        ">
+            
+            <div class="woocommerce-info">
+                Instructions on how to measure below.
+            </div>
+        </div>
+    `;
+
+    if(!target) {
+        return;
+    } else {
+        target.insertAdjacentHTML('afterend', template);
+    }
+};
+
+function sizeChartText(e) {
+    const title = document.querySelector('.description_tab a');
+
+    if (title) {
+        title.innerHTML = 'Size Chart';
+    }
+};
+
 $(document).ready(function() {
     buildFAQ();
+	addSizeInfoCheckout();
+	sizeChartText();
 });
