ba70a4b02cd486d856bc0cc5b5d5692610f5c167
K1NGAARON <gerardaaron1997@gmail.com>
2023-10-27 10:24:47 +0200

    chanegs

---
+++ testronic/script.js
@@ -0,0 +1,18 @@
+function closeAccordion(e) {
+    $('.accordion-head').removeClass('active');
+    $('.accordion-body').removeClass('active');
+    $('.accordion-head').children('.icon').removeClass('active');
+    $('.accordion-head').children('.icon').children('i').removeClass('fa-minus');
+    $('.accordion-head').children('.icon').children('i').addClass('fa-plus');
+};
+
+$('.accordion-item').click(function () {
+    closeAccordion();
+
+    $(this).children('.accordion-head').toggleClass('active');
+    $(this).children('.accordion-head').siblings('.accordion-body').toggleClass('active');
+    $(this).children('.accordion-head').children('.icon').toggleClass('active');
+
+    $(this).children('.accordion-head').children('.icon').children('i').removeClass('fa-plus');
+    $(this).children('.accordion-head').children('.icon').children('i').addClass('fa-minus');
+});
