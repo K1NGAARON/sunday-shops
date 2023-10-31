0d6ea928e58a58bb68e5ddb618febab5b8000d6f
K1NGAARON <gerardaaron1997@gmail.com>
2023-10-31 13:38:30 +0100

    io code

--- io/global.js
+++ io/global.js
@@ -29,7 +29,10 @@ function replaceErrorText(e) {
     const errorMSG = document.querySelector('#payment .woocommerce-info');
 
     if (errorMSG) {
-        errorMSG.innerText = "Don’t forget to add your Coupon Code to be able to order your iO goodies!";
+        errorMSG.innerText = `Don’t forget to add your Coupon Code to be able to order your iO goodies!
+								
+								Also keep in mind you're limited to 3 items.
+                            `;
         errorMSG.classList.add('active');
     }
 }
@@ -139,4 +142,83 @@ $(document).ready(function() {
         hideCheckoutCount();
         replaceErrorText();
     }, 1000);
+});
+
+$(document).ready(function() {
+    const variationsElement = document.querySelector('.woocommerce-product-details__short-description');
+
+    if (variationsElement) {
+        const oneSizeProductsURL = [
+            'https://iogoodieworld.shop.teamsunday.com/product/io-cap/',
+            'https://iogoodieworld.shop.teamsunday.com/product/io-socks/',
+            'https://iogoodieworld.shop.teamsunday.com/product/io-bottle-orange/',
+            'https://iogoodieworld.shop.teamsunday.com/product/io-bottle-white/',
+            'https://iogoodieworld.shop.teamsunday.com/product/io-sports-shirt/',
+            'https://iogoodieworld.shop.teamsunday.com/product/io-sports-shirt-purple/',
+            'https://iogoodieworld.shop.teamsunday.com/product/io-totebag/'
+        ];
+
+        const currentURL = window.location.href;
+
+        if (oneSizeProductsURL.includes(currentURL)) {
+            return;
+        } else {
+            const anchorElement = `
+                <div class="sizer-holder">
+                    <a id="openSizer">Calculate Size</a>
+                </div>
+            `;
+            variationsElement.insertAdjacentHTML('beforeend', anchorElement);
+        }
+    }
+
+    function waitForElementToExist(elementId, callback) {
+        const targetNode = document.documentElement;
+        const config = { childList: true, subtree: true };
+      
+        const observer = new MutationObserver(function(mutationsList, observer) {
+            for (let mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    const element = document.getElementById(elementId);
+                    if (element) {
+                        observer.disconnect();
+                        callback(element);
+                    }
+                }
+            }
+        });
+      
+        observer.observe(targetNode, config);
+    }
+
+    waitForElementToExist('openSizer', function(e) {
+        const modal = document.querySelector('#myModal');
+        const openSizer = document.querySelector('#openSizer');
+
+        function showPopUp(e) {
+            modal.style.display = "block";
+        }
+    
+        function hidePopUp(e) {
+            modal.style.display = "none";
+        }
+    
+        let span = document.getElementById("closeModal");
+        
+        $(window).click((e) => {
+            if (e.target == modal) {
+                hidePopUp();
+            }
+        })
+
+        $(window).on('keyup', function(event) {
+            if (event.key == "Escape") {
+                hidePopUp();
+            }
+        });
+
+        // Modal functions 
+        openSizer.addEventListener('click', showPopUp);
+        span.addEventListener('click', hidePopUp);
+    })
 });
