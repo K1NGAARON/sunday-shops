b123fb0308b08d0611be95a687b180773a3d3c73
K1NGAARON <gerardaaron1997@gmail.com>
2023-11-27 10:30:20 +0100

    changes

--- io-gift/global.js
+++ io-gift/global.js
@@ -22,8 +22,8 @@ function replaceErrorText(e) {
 
     if (errorMSG) {
         errorMSG.innerText = 
-            `Don’t forget to add your Coupon Code to be able to order your end of year gift.
-            Also don't forget you're limited to 1 free item!
+            `Don't forget to add your Voucher code to be able to order your End of Year gift.
+				Also keep in mind you're limited to 1 item.
             `;
         errorMSG.classList.add('active');
     }
@@ -54,8 +54,8 @@ function sizeChartText(e) {
     
     function checkAndHideOneSizeTable() {
         const oneSizeProductsURL = [
-            'https://io-gift.shop.teamsunday.com/product/backpack/',
-			'https://io-gift.shop.teamsunday.com/product/laptop-sleeve/'
+            'https://io-gift.shop.teamsunday.com/product/io-backpack/',
+			'https://io-gift.shop.teamsunday.com/product/io-laptop-sleeve/'
         ];
     
         const currentURL = window.location.href;
@@ -83,78 +83,4 @@ $(document).ready(function() {
 	setTimeout(function() {
         replaceErrorText();
     }, 1000);
-});
-
-$(document).ready(function() {
-    const variationsElement = document.querySelector('.woocommerce-product-details__short-description');
-
-    if (variationsElement) {
-        const oneSizeProductsURL = [
-            'https://io-gift.shop.teamsunday.com/product/backpack/',
-			'https://io-gift.shop.teamsunday.com/product/laptop-sleeve/'
-        ];
-
-        const currentURL = window.location.href;
-
-        if (oneSizeProductsURL.includes(currentURL)) {
-            return;
-        } else {
-            const anchorElement = `
-                <div class="sizer-holder">
-                    <a id="openSizer">Calculate Size</a>
-                </div>
-            `;
-            variationsElement.insertAdjacentHTML('beforeend', anchorElement);
-        }
-    }
-
-    function waitForElementToExist(elementId, callback) {
-        const targetNode = document.documentElement;
-        const config = { childList: true, subtree: true };
-      
-        const observer = new MutationObserver(function(mutationsList, observer) {
-            for (let mutation of mutationsList) {
-                if (mutation.type === 'childList') {
-                    const element = document.getElementById(elementId);
-                    if (element) {
-                        observer.disconnect();
-                        callback(element);
-                    }
-                }
-            }
-        });
-      
-        observer.observe(targetNode, config);
-    }
-
-    waitForElementToExist('openSizer', function(e) {
-        const modal = document.querySelector('#myModal');
-        const openSizer = document.querySelector('#openSizer');
-
-        function showPopUp(e) {
-            modal.style.display = "block";
-        }
-    
-        function hidePopUp(e) {
-            modal.style.display = "none";
-        }
-    
-        let span = document.getElementById("closeModal");
-        
-        $(window).click((e) => {
-            if (e.target == modal) {
-                hidePopUp();
-            }
-        })
-
-        $(window).on('keyup', function(event) {
-            if (event.key == "Escape") {
-                hidePopUp();
-            }
-        });
-
-        // Modal functions 
-        openSizer.addEventListener('click', showPopUp);
-        span.addEventListener('click', hidePopUp);
-    })
 });
