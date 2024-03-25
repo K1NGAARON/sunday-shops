af2985e7b7c8bc8ed7d0c892ea631201af14fb2a
K1NGAARON <gerardaaron1997@gmail.com>
2024-03-25 10:23:50 +0100

    changes

---
+++ proximus/global.js
@@ -0,0 +1,31 @@
+function closeAccordion(e) {
+    $('.accordion-head').removeClass('active');
+    $('.accordion-body').removeClass('active');
+    $('.accordion-head').children('.icon').removeClass('active');
+    $('.accordion-head').children('.icon').children('i').removeClass('fa-minus');
+    $('.accordion-head').children('.icon').children('i').addClass('fa-plus');
+};
+
+$('.accordion-item').click(function () {
+    closeAccordion();
+
+    $(this).children('.accordion-head').toggleClass('active');
+    $(this).children('.accordion-head').siblings('.accordion-body').toggleClass('active');
+    $(this).children('.accordion-head').children('.icon').toggleClass('active');
+
+    $(this).children('.accordion-head').children('.icon').children('i').removeClass('fa-plus');
+    $(this).children('.accordion-head').children('.icon').children('i').addClass('fa-minus');
+});
+
+function addTextToCheckout() {
+    if (window.location.href.indexOf("/checkout/order-received/") > -1) {
+        $('.woocommerce-notice.woocommerce-notice--success.woocommerce-thankyou-order-received').text("Thank you for ordering on the Proximus shop!");
+       
+        let successNotice = $(".woocommerce-notice.woocommerce-notice--success.woocommerce-thankyou-order-received");
+        successNotice.after("<p class='track-trace'>You will receive a track & trace once your order is shipped.</p>");
+    };
+};
+
+$(document).ready(function() {
+    addTextToCheckout()
+});
