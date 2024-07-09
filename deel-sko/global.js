9e6dd639ec085ff381153894b12a1bb89b391c87
K1NGAARON <gerardaaron1997@gmail.com>
2024-07-09 09:39:10 +0200

    optimized code

--- deel-sko/global.js
+++ deel-sko/global.js
@@ -1,20 +1,20 @@
-function closeAccordion(e) {
+function closeAccordion() {
     $('.accordion-head').removeClass('active');
     $('.accordion-body').removeClass('active');
-    $('.accordion-head').children('.icon').removeClass('active');
-    $('.accordion-head').children('.icon').children('i').removeClass('fa-minus');
-    $('.accordion-head').children('.icon').children('i').addClass('fa-plus');
-};
+    $('.accordion-head .icon').removeClass('active').children('i').removeClass('fa-minus').addClass('fa-plus');
+}
 
 $('.accordion-item').click(function () {
     closeAccordion();
 
-    $(this).children('.accordion-head').toggleClass('active');
-    $(this).children('.accordion-head').siblings('.accordion-body').toggleClass('active');
-    $(this).children('.accordion-head').children('.icon').toggleClass('active');
+    let head = $(this).children('.accordion-head');
+    let icon = head.children('.icon');
+    let iconChild = icon.children('i');
 
-    $(this).children('.accordion-head').children('.icon').children('i').removeClass('fa-plus');
-    $(this).children('.accordion-head').children('.icon').children('i').addClass('fa-minus');
+    head.toggleClass('active');
+    head.siblings('.accordion-body').toggleClass('active');
+    icon.toggleClass('active');
+    iconChild.removeClass('fa-plus').addClass('fa-minus');
 });
 
 const oneSizeProductsURL = [];
