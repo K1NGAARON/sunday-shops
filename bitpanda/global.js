550b16507941f01be58e98c29d81be4056331c3b
K1NGAARON <gerardaaron1997@gmail.com>
2024-07-15 12:58:17 +0200

    changes

--- bitpanda/global.js
+++ bitpanda/global.js
@@ -1,21 +1,23 @@
-function closeAccordion(e) {
+function closeAccordion() {
     $('.accordion-head').removeClass('active');
     $('.accordion-body').removeClass('active');
     $('.accordion-head').children('.icon').removeClass('active');
-    $('.accordion-head').children('.icon').children('i').removeClass('fa-minus');
-    $('.accordion-head').children('.icon').children('i').addClass('fa-plus');
-};
+    $('.accordion-head').children('.icon').children('i').removeClass('fa-minus').addClass('fa-plus');
+}
 
 $('.accordion-item').click(function () {
+    let isActive = $(this).children('.accordion-head').hasClass('active');
+    
     closeAccordion();
 
-    $(this).children('.accordion-head').toggleClass('active');
-    $(this).children('.accordion-head').siblings('.accordion-body').toggleClass('active');
-    $(this).children('.accordion-head').children('.icon').toggleClass('active');
-
-    $(this).children('.accordion-head').children('.icon').children('i').removeClass('fa-plus');
-    $(this).children('.accordion-head').children('.icon').children('i').addClass('fa-minus');
+    if (!isActive) {
+        $(this).children('.accordion-head').toggleClass('active');
+        $(this).children('.accordion-head').siblings('.accordion-body').toggleClass('active');
+        $(this).children('.accordion-head').children('.icon').toggleClass('active');
+        $(this).children('.accordion-head').children('.icon').children('i').removeClass('fa-plus').addClass('fa-minus');
+    }
 });
+
 
 
 function addPriceIcon(e) {
