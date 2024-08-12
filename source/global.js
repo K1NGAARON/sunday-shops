cb23014562c497de637eb3ee4e380dd1e18096de
K1NGAARON <gerardaaron1997@gmail.com>
2024-08-12 13:09:55 +0200

    changes

--- source/global.js
+++ source/global.js
@@ -35,6 +35,103 @@ function addPriceIcon(e) {
     };
 };
 
+const oneSizeProductsURL = [];
+
+function sizeChartText(e) {
+    function hideOneSizeTable() {
+        const table = document.querySelector('.variations');
+        $(table).hide();
+    };
+    
+    function checkAndHideOneSizeTable() {
+        const currentURL = window.location.href;
+      
+        if (oneSizeProductsURL.includes(currentURL)) {
+            hideOneSizeTable();
+            $('.sizer-holder').hide();
+        }
+    };
+
+    const title = document.querySelector('.description_tab a');
+
+    if (title) {
+        title.innerHTML = 'Size Chart';
+    }
+
+    checkAndHideOneSizeTable();
+}
+
 $(document).ready(function() {
-	addPriceIcon()
+    const variationsElement = document.querySelector('.woocommerce-product-details__short-description');
+
+    if (variationsElement) {
+        const currentURL = window.location.href;
+
+        if (oneSizeProductsURL.includes(currentURL)) {
+            return;
+        } else {
+            const anchorElement = `
+                <div class="sizer-holder">
+                    <a id="openSizer">Calculate Size</a>
+                </div>
+            `;
+            variationsElement.insertAdjacentHTML('beforeend', anchorElement);
+        }
+    }
+
+    function waitForElementToExist(elementId, callback) {
+        const targetNode = document.documentElement;
+        const config = { childList: true, subtree: true };
+      
+        const observer = new MutationObserver(function(mutationsList, observer) {
+            for (let mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    const element = document.getElementById(elementId);
+                    if (element) {
+                        observer.disconnect();
+                        callback(element);
+                    }
+                }
+            }
+        });
+      
+        observer.observe(targetNode, config);
+    }
+
+    waitForElementToExist('openSizer', function(e) {
+        const modal = document.querySelector('#myModal');
+        const openSizer = document.querySelector('#openSizer');
+
+        function showPopUp(e) {
+            modal.style.display = "block";
+        }
+    
+        function hidePopUp(e) {
+            modal.style.display = "none";
+        }
+    
+        let span = document.getElementById("closeModal");
+        
+        $(window).click((e) => {
+            if (e.target == modal) {
+                hidePopUp();
+            }
+        })
+
+        $(window).on('keyup', function(event) {
+            if (event.key == "Escape") {
+                hidePopUp();
+            }
+        });
+
+        // Modal functions 
+        openSizer.addEventListener('click', showPopUp);
+        span.addEventListener('click', hidePopUp);
+    })
+});
+
+
+$(document).ready(function() {
+	addPriceIcon();
+    sizeChartText();
 });
