1a8ffdcd429942de5f877ff1d5f7d5406c0d9add
K1NGAARON <gerardaaron1997@gmail.com>
2024-08-13 18:14:27 +0200

    changes

--- tts/global.js
+++ tts/global.js
@@ -1,22 +1,32 @@
 function truncateProductDescription() {
-    const charactersToShow = 80;
+    const charactersToShow = 120;
     const ellipses = "... ";
 
-    let content = $(".read-more-content .elementor-widget-container").html();
+    let totalContentLength = 0;
+    const pElements = $(".read-more-content .elementor-widget-container p");
 
-    if (content.length > charactersToShow) {
-        let a = content.substr(0, charactersToShow);
-        let b = content.substr(charactersToShow - content.length);
-        let html = a + "<span class='truncated'>" + ellipses + "<a class='read-more'>Read more</a></span><span class='truncated' style='display:none'>" + b + "</span>";
-        
-        $(".read-more-content .elementor-widget-container").html(html);
-    }
-
-
-    $(".read-more").click(function(e) {
-        e.preventDefault();
-        $(".read-more-content .elementor-widget-container .truncated").toggle();
+    // Calculate the total content length of all <p> elements combined
+    pElements.each(function() {
+        totalContentLength += $(this).text().length;
     });
-};
+
+    // If the total content length exceeds the limit, hide all <p> elements and add a "Read more" button
+    if (totalContentLength > charactersToShow) {
+        pElements.hide();
+
+        // Show the first paragraph truncated with the ellipsis and add the "Read more" button
+        const firstPContent = pElements.first().text().substr(0, charactersToShow) + ellipses;
+        pElements.first().html(firstPContent).show().append("<a class='read-more' style='display:block; cursor:pointer;'>Lees verder</a>");
+
+        // Event listener for the "Read more" link
+        $(".read-more").click(function(e) {
+            e.preventDefault();
+
+            // Show all hidden <p> elements and remove the "Read more" button
+            pElements.show();
+            $(this).remove();
+        });
+    }
+}
 
 $(document).ready(truncateProductDescription);
