e0310db23a3c2168f72aed7fc9dcf033ce910987
K1NGAARON <gerardaaron1997@gmail.com>
2024-08-15 15:41:20 +0200

    changes

--- tts/global.js
+++ tts/global.js
@@ -1,22 +1,28 @@
 function shortenText() {
-    const charactersToShow = 120;
-    const ellipses = "... ";
+    const description = $('.product .read-more-content .elementor-widget-container');
+    console.log(description);
     
-    const description = $('.read-more-content .elementor-widget-container');
-    const originalContent = description.html();
-    const descriptionLength = $(description).text().length;
-    
-    console.log('Total Content length: ', descriptionLength);
-    
-    if (descriptionLength > charactersToShow) {
-        const firstContent = description.text().substr(0, charactersToShow) + ellipses;
-        description.html(firstContent).append("<a class='read-more-btn' style='display:block; cursor:pointer;'>Lees verder</a>");
+    if (!description) {
+        return;
+    } else {
+        const charactersToShow = 120;
+        const ellipses = "... ";
 
-        console.log('First content to show:', firstContent);
-
-        $('.read-more-btn').click(function() {
-            description.html(originalContent);
-        });
+        const originalContent = description.html();
+        const descriptionLength = $(description).text().length;
+        
+        console.log('Total Content length: ', descriptionLength);
+        
+        if (descriptionLength > charactersToShow) {
+            const firstContent = description.text().substr(0, charactersToShow) + ellipses;
+            description.html(firstContent).append("<a class='read-more-btn' style='display:block; cursor:pointer;'>Lees verder</a>");
+    
+            console.log('First content to show:', firstContent);
+    
+            $('.read-more-btn').click(function() {
+                description.html(originalContent);
+            });
+        }
     }
 }
 
